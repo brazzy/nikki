bc32ae250332c1dcb8f8ef06cf890b239a1d179f
Michael Borgwardt <brazzy@gmail.com>
2009-09-13 12:56:06 +0000

    Speichern des Auswahlverzeichnisses als Perferences
    git-svn-id: svn://localhost/map_kmz/trunk@88 4994fb73-42c4-f447-92f5-66ae8ea45fe7

--- src/groovy/de/brazzy/nikki/model/NikkiModel.groovy
+++ src/groovy/de/brazzy/nikki/model/NikkiModel.groovy
@@ -1,10 +1,16 @@
 package de.brazzy.nikki.model
 
-
+import java.util.prefs.Preferences
 /**
  * @author Michael Borgwardt
  *
  */
 public class NikkiModel extends ListDataModel<Directory>{    
-    File selectionDir
+    File selectionDir = new File(Preferences.userNodeForPackage(this.class).get("selectionDir", null))
+    
+    void setSelectionDir(File f)
+    {
+        Preferences.userNodeForPackage(this.class).put("selectionDir", f.getAbsolutePath())
+        this.selectionDir = f;
+    }
 }
