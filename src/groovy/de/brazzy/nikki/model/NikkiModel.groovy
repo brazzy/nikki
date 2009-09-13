882730f0fbf749104963b38b67f28332c10a9e6e
Michael Borgwardt <brazzy@gmail.com>
2009-09-13 13:16:10 +0000

    Ausgewählte Verzeichnisse als Prefs speichern
    git-svn-id: svn://localhost/map_kmz/trunk@89 4994fb73-42c4-f447-92f5-66ae8ea45fe7

--- src/groovy/de/brazzy/nikki/model/NikkiModel.groovy
+++ src/groovy/de/brazzy/nikki/model/NikkiModel.groovy
@@ -6,11 +6,47 @@ import java.util.prefs.Preferences
  *
  */
 public class NikkiModel extends ListDataModel<Directory>{    
-    File selectionDir = new File(Preferences.userNodeForPackage(this.class).get("selectionDir", null))
+    public static final String PREF_KEY_DIRECTORIES = "directories"
+    public static final String PREF_KEY_SELECTION_DIR = "selectionDir"
+    public static final String SEP = System.getProperty("path.separator")
     
-    void setSelectionDir(File f)
+    static Preferences prefs = Preferences.userNodeForPackage(NikkiModel.class)
+    
+    File selectionDir
+    public void setSelectionDir(File f)
     {
-        Preferences.userNodeForPackage(this.class).put("selectionDir", f.getAbsolutePath())
+        Preferences.userNodeForPackage(this.class).put(PREF_KEY_SELECTION_DIR, f.absolutePath)
         this.selectionDir = f;
     }
+
+    public NikkiModel()
+    {
+        selectionDir = new File(prefs.get(PREF_KEY_SELECTION_DIR, null))        
+        def dirs = prefs.get(PREF_KEY_DIRECTORIES, null);
+        if(dirs)
+        {
+            dirs = dirs.split(SEP)
+            dirs.each{
+                if(it && it.length()>0)
+                {
+                    super.add(new Directory(path: new File(it)))
+                }
+            }            
+        }
+    }
+    
+    
+    public void add(Directory d)
+    {
+        super.add(d)
+        def dirs = prefs.get(PREF_KEY_DIRECTORIES, SEP);
+        prefs.put(PREF_KEY_DIRECTORIES, dirs+d.path.absolutePath+SEP)
+    }
+    public boolean remove(Directory d)
+    {
+        def dirs = prefs.get(PREF_KEY_DIRECTORIES, "");
+        prefs.put(PREF_KEY_DIRECTORIES, dirs.replace(SEP+d.path.absolutePath+SEP, SEP))
+        return super.remove(d)
+    }
+
 }
