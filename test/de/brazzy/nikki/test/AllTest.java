475b493687b02f50f1c1f1b77e8f0d00889a6166
Michael Borgwardt <brazzy@gmail.com>
2009-12-11 21:59:36 +0000

    Testsuite umbenannt um automatisches Ausführen in Netbeans zu ermöglichen
    git-svn-id: svn://localhost/map_kmz/trunk@167 4994fb73-42c4-f447-92f5-66ae8ea45fe7

--- test/de/brazzy/nikki/test/AllTest.java
+++ test/de/brazzy/nikki/test/AllTest.java
@@ -2,11 +2,11 @@ package de.brazzy.nikki.test;
 
 import junit.framework.TestSuite;
 
-public class NikkiTestSuite extends TestSuite{
+public class AllTest extends TestSuite{
 
     public static TestSuite suite()
     {
-        TestSuite s=new NikkiTestSuite();
+        TestSuite s=new AllTest();
         s.addTestSuite(RelativeDateFormatTest.class);
         s.addTestSuite(WaypointTest.class);
         s.addTestSuite(NikkiModelTest.class);
