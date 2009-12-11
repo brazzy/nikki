8a5bc1665dd613c40500a9311b1a8c5f1e275c11
Michael Borgwardt <brazzy@gmail.com>
2009-12-11 19:22:53 +0000

    Parsingfehler korrigiert
    git-svn-id: svn://localhost/map_kmz/trunk@163 4994fb73-42c4-f447-92f5-66ae8ea45fe7

--- src/de/brazzy/nikki/model/GeoCoordinate.groovy
+++ src/de/brazzy/nikki/model/GeoCoordinate.groovy
@@ -22,9 +22,9 @@ public class GeoCoordinate implements Serializable{
             throw new IllegalArgumentException(dir)
         }
         def spl = mag.tokenize(".")
-        int degrees = Integer.parseInt(spl[0].substring(0, spl[0].length()-2))
+        def degrees = Integer.parseInt(spl[0].substring(0, spl[0].length()-2))
         double minutes = Double.parseDouble(spl[0].substring(spl[0].length()-2, spl[0].length())+"."+spl[1])
-        degrees += minutes / 60.0
+        degrees += minutes / 60.0d
         def result = new GeoCoordinate(direction: Cardinal.parse(dir), magnitude: degrees)
         return result;
     }
