188607dee7b71b795e5f1cda0567f77361c6fc6c
Michael Borgwardt <brazzy@gmail.com>
2009-12-11 23:10:45 +0000

    neu: testRemove()
    git-svn-id: svn://localhost/map_kmz/trunk@168 4994fb73-42c4-f447-92f5-66ae8ea45fe7

--- test/de/brazzy/nikki/test/NikkiModelTest.groovy
+++ test/de/brazzy/nikki/test/NikkiModelTest.groovy
@@ -2,6 +2,7 @@ package de.brazzy.nikki.test
 
 import de.brazzy.nikki.Nikki
 import de.brazzy.nikki.model.NikkiModel
+import de.brazzy.nikki.model.Directory
 import de.brazzy.nikki.view.NikkiFrame
 
 /**
@@ -30,6 +31,17 @@ class NikkiModelTest extends GroovyTestCase {
         view.addButton.actionListeners[0].actionPerformed()
         assertEquals(1, model.size())
         assertEquals("tmp (0, 0)", model[0].toString())
+    }
+
+    public void testRemove()
+    {
+        model.add(new Directory(path: new File("C:\\tmp1")))
+        model.add(new Directory(path: new File("C:\\tmp2")))
+        assertEquals(2, model.size())
+        view.dirList.selectedIndex = 0
+        view.deleteButton.actionListeners[0].actionPerformed()
+        assertEquals(1, model.size())
+        assertEquals("tmp2 (0, 0)", model[0].toString())
     }
 }
 
