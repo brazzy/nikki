be74b687ac5b5af6829f20cb158d6bf0d0dca151
Michael Borgwardt <brazzy@gmail.com>
2010-03-20 20:40:47 +0000

    make comparator serializable
    git-svn-id: svn://localhost/nikki/trunk@272 4994fb73-42c4-f447-92f5-66ae8ea45fe7

--- src/de/brazzy/nikki/util/WaypointComparator.java
+++ src/de/brazzy/nikki/util/WaypointComparator.java
@@ -1,11 +1,14 @@
 package de.brazzy.nikki.util;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
 import de.brazzy.nikki.model.Waypoint;
 
-public class WaypointComparator implements Comparator<Waypoint>
+public class WaypointComparator implements Comparator<Waypoint>, Serializable
 {
+    public static final long serialVersionUID = 1L;
+
     @Override
     public int compare(Waypoint o1, Waypoint o2)
     {
