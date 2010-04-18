b1e67be7ed595ce29f3592ef6cfd6b8d200ff515
Michael Borgwardt <brazzy@gmail.com>
2010-04-18 22:04:19 +0200

    Intrantionalization infrastructure added

---
+++ src/de/brazzy/nikki/Texts.java
@@ -0,0 +1,119 @@
+package de.brazzy.nikki;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Locale;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+/*   
+ *   Copyright 2010 Michael Borgwardt
+ *   Part of the Nikki Photo GPS diary:  http://www.brazzy.de/nikki
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+/**
+ * Holds internationalized texts.
+ * 
+ * @author Michael Borgwardt
+ *
+ */
+public final class Texts
+{
+    private static final ResourceBundle bundle = ResourceBundle.getBundle("de.brazzy.nikki.Texts", new ResourceControl());
+    
+    public static final String ERROR_PREFIX = bundle.getString("general.error_prefix");
+    
+    
+    public static class Main
+    {
+        public static final String TITLE = bundle.getString("mainwindow.title");        
+        public static final String DIRECTORIES = bundle.getString("mainwindow.directories");
+        public static final String DAYS = bundle.getString("mainwindow.days");
+        public static final String IMAGES = bundle.getString("mainwindow.images");
+        public static final String UNKNOWN_DAY = bundle.getString("mainwindow.unknown_day");
+        public static final String ADD_BUTTON = bundle.getString("mainwindow.add_button");
+        public static final String ADD_TOOLTIP = bundle.getString("mainwindow.add_tooltip");
+        public static final String DELETE_BUTTON = bundle.getString("mainwindow.delete_button");
+        public static final String DELETE_TOOLTIP = bundle.getString("mainwindow.delete_tooltip");
+        public static final String SCAN_BUTTON = bundle.getString("mainwindow.scan_button");
+        public static final String SCAN_TOOLTIP = bundle.getString("mainwindow.scan_tooltip");
+        public static final String SAVE_BUTTON = bundle.getString("mainwindow.save_button");
+        public static final String SAVE_TOOLTIP = bundle.getString("mainwindow.save_tooltip");
+        public static final String GEOTAG_BUTTON = bundle.getString("mainwindow.geotag_button");
+        public static final String GEOTAG_TOOLTIP = bundle.getString("mainwindow.geotag_tooltip");
+        public static final String EXPORT_BUTTON = bundle.getString("mainwindow.export_button");
+        public static final String EXPORT_TOOLTIP = bundle.getString("mainwindow.export_tooltip");
+        public static final String HELP_TOOLTIP = bundle.getString("mainwindow.help_tooltip");
+        
+    }
+    
+    public static class Image
+    {
+        public static final String FILE = bundle.getString("image.file");
+        public static final String TIME = bundle.getString("image.time");
+        public static final String LATITUDE = bundle.getString("image.latitude");
+        public static final String LONGITUDE = bundle.getString("image.longitude");        
+        public static final String OFFSET_ERROR_TITLE = bundle.getString("image.offset_error_title");
+        public static final String DIFF_TOOLTIP = bundle.getString("image.diff_tooltip");
+    }
+    
+    
+    public static class Dialogs
+    {
+        public static final String CONFIRM_TITLE = bundle.getString("dialogs.confirm_title");
+        public static final String UNSAVED_MESSAGE = bundle.getString("dialogs.unsaved_message");
+        
+        public static class About
+        {
+
+            public static final String TITLE = bundle.getString("dialogs.about.title");
+            public static final String FILENAME = bundle.getString("dialogs.about.filename");
+            
+        }
+        public static class ScanOptions
+        {
+
+            public static final String TITLE = bundle.getString("dialogs.scan_option.title");
+            public static final String TIMEZONE_LABEL = bundle.getString("dialogs.scan_option.timezone_label");
+            
+        }
+        public static class GeotagOptions
+        {
+
+            public static final String TITLE = bundle.getString("dialogs.geotag_option.title");
+            public static final String OFFSET_LABEL = bundle.getString("dialogs.geotag_option.offset_label");
+            
+        }
+    }
+    
+    /**
+     * Enables the loading of property files encoded in UTF-8
+     * 
+     * @author Michael Borgwardt
+     */
+    private static class ResourceControl extends ResourceBundle.Control
+    {
+        @Override
+        public ResourceBundle newBundle(String baseName, Locale locale, String format, ClassLoader loader,
+                boolean reload) throws IllegalAccessException, InstantiationException, IOException
+        {
+            String bundlename = toBundleName(baseName, locale);
+            String resName = toResourceName(bundlename, "properties");
+            InputStream stream = loader.getResourceAsStream(resName);
+            return new PropertyResourceBundle(new InputStreamReader(stream, "UTF-8"));
+        }
+        
+    }
+}
